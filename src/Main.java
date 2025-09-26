2c3e7cdd2bb465f0e642cc97669834d7913afaa1
mIINguta <gustavo.minguta@gmail.com>
2025-09-26 14:56:49 -0300

    Realizando leitura de teclado

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-      int num = 0;
-      String nome = "Gustavo Minguta";
+    // realizando leitura
+        //No java, é necessário criar um objeto Scanner para ler as linhas, do C# que usa console.readLine();
+        Scanner scan = new Scanner(System.in);
 
-        System.out.println("Hello world!");
-        System.out.printf("Valor da variável num: %d\nNome: %s", num, nome);
+        int n1, n2, res = 0;
+        String nome = "";
+
+        System.out.println("Digite seu nome: ");
+        nome = scan.nextLine();
+
+        System.out.println("Digite um numeral: ");
+        n1 = scan.nextInt();
+
+        System.out.println("Digite outro numeral: ");
+        n2 = scan.nextInt();
+
+        res = n1 + n2;
+
+        System.out.printf("%s, o resultado da soma dos numerais é: %d ",  nome,  res);
+
+
     }}
