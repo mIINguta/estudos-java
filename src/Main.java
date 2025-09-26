dc5e24ae60592ccf337bcd9b2c91de97070220cb
mIINguta <gustavo.minguta@gmail.com>
2025-09-26 10:41:33 -0300

    Primeiros comandos com Java (prints + criação de variáveis)

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      int num = 0;
+      String nome = "Gustavo Minguta";
+
+        System.out.println("Hello world!");
+        System.out.printf("Valor da variável num: %d\nNome: %s", num, nome);
+    }}
